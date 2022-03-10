ac869b08886a12bdd9c6047a74b36720a99c2a3e
JoNoC0 <jnguyeningco@gmail.com>
2022-03-09 20:36:34 -0600

    import nav icons

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './Nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri';
+import {BiMessageSquareDetail} from 'react-icons/bi'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#'><AiOutlineHome /></a>
+      <a href='#About'><AiOutlineUser /></a>
+      <a href='#Experience'><BiBook /></a>
+      <a href='#Services'><RiServiceLine /></a>
+      <a href='#Contacts'><BiMessageSquareDetail /></a>
+    </nav>
   )
 }
 
