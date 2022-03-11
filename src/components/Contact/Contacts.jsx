5ed28eac91c9e7d76168b981751c1b8372452dd1
JoNoC0 <jnguyeningco@gmail.com>
2022-03-11 00:34:28 -0600

    created contact email and messenger

--- src/components/Contact/Contacts.jsx
+++ src/components/Contact/Contacts.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
-import './Contacts.css'
+import React from 'react';
+import './Contacts.css';
+import {MdOutlineEmail} from 'react-icons/md';
+import { RiMessengerLine } from 'react-icons/ri';
 
 export const Contacts = () => {
   return (
-    <section id='contact'>Contact</section> 
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail />
+            <h4>Email</h4>
+            <h5>JNguyeningCo@gmail.com</h5>
+            <a href="mailto:JNguyeningCo@gmail.com">Send a message</a>
+          </article>
+          
+
+          <article className="contact__option">
+            <RiMessengerLine />
+            <h4>Messenger</h4>
+            <h5>John Web Dev</h5>
+            <a href="https://m.me/HoaJohnWinn">Send a message</a>
+          </article>  
+
+          {/* <article className="contact__option">
+            <RiMessengerLine />
+            <h4>Email</h4>
+            <h5>JNguyeningCo@gmail.com</h5>
+            <a href="mailto:JNguyeningCo@gmail.com">Send a message</a>
+          </article>     */}
+        </div>  
+        {/* ====== END OF CONTACT OPTIONS ====== */}
+        <form action=""></form>
+      </div>
+    </section> 
   )
 }
 
