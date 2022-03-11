2076a7296b4435a1673e1eb86f6320f3829f77b1
JoNoC0 <jnguyeningco@gmail.com>
2022-03-11 01:48:34 -0600

    created contacts jsx and css installed email js created contact form

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
         <CTA />
         <HeaderSocials />
         <div className="me">
-          <img src={ME} alt='me' />
+          <img src={ME} alt='John' />
         </div>
 
         <a href='#contacts' className='scroll__down'>Scroll Down</a>
