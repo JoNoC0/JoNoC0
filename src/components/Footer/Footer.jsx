26f0c228d7f5b2ad29dbb6371e803dd2be12c29d
JoNoC0 <jnguyeningco@gmail.com>
2022-03-11 02:20:33 -0600

    created footer jsx and css changed color to navbar

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
-import './Footer.css'
+import React from 'react';
+import './Footer.css';
+import { BsLinkedin } from 'react-icons/bs';
+import { BsGithub } from 'react-icons/bs';
+import { BsTwitter } from 'react-icons/bs';
 
 const Footer = () => {
+    let date = new Date();
+    let year = date.getFullYear();
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>JNguyen</a>
+
+      <ul className="permalinks">
+        <li><a href="#"Home></a></li>
+        <li><a href="#about"About></a></li>
+        <li><a href="#"experience>Experience</a></li>
+        <li><a href="#"services>Services</a></li>
+        <li><a href="#"portfolio>Portfolio</a></li>
+        <li><a href="#"contacts>Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href='https://wwww.linkedin.com' target='_blank'><BsLinkedin /></a>
+        <a href='https://wwww.github.com' target='_blank'><BsGithub /></a>
+        <a href='https://wwww.twitter.com' target='_blank'><BsTwitter /></a>    
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; {year} John Nguyen. All rights reserved.</small>
+        <small>special thanks to EGATOR</small>
+      </div>
+    </footer>
   )
 }
 
